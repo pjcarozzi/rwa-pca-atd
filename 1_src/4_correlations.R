57e19f271bfb32d11c475d27f27d5cafd7a66a93
Rocío Salas-Lewin <rsalaslewin@gmail.com>
2023-03-09 17:58:07 -0300

    cor-p

--- 1_src/4_correlations.R
+++ 1_src/4_correlations.R
@@ -21,17 +21,21 @@ data2 <- import("0_data/data_sel_v2.csv") %>%
 
 ###---CORRELATION MATRIX.
 
-cor1 <- as.data.frame(cor(data1,
-                         use = "pairwise.complete.obs",
-                         method = "pearson"))
+cor1 <- corr.test(data1, use = "pairwise",method="pearson")
+pearson1 <- as.data.frame(cor1$r)
+p1 <- as.data.frame(cor1$p)
 
-cor2 <- as.data.frame(cor(data2,
-                          use = "pairwise.complete.obs",
-                          method = "pearson"))
+cor2 <- corr.test(data2, use = "pairwise",method="pearson")
+pearson2 <- as.data.frame(cor2$r)
+p2 <- as.data.frame(cor2$p)
 
-export(cor1, "2_output/1_alpha/correlations_v1.xlsx", rowNames = T)
+sheets1 <- list("Cor" = pearson1, "P" = p1)
+export(sheets1, "2_output/1_alpha/correlations_v1.xlsx", rowNames = T)
 
-export(cor2, "2_output/1_alpha/correlations_v2.xlsx", rowNames = T)
+sheets2 <- list("Cor" = pearson2, "P" = p2)
+export(sheets2, "2_output/1_alpha/correlations_v2.xlsx", rowNames = T)
+
+
 
 
 
